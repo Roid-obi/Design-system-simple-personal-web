2620d58c01bdf4ae6810aa67740b57ce9eb7bb95
Roid-obi <contohdiamazon2@gmail.com>
2024-08-13 21:42:14 +0700

    first commit

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,14 @@
+import { Button } from './Button';
+
+const meta = {
+  component: Button,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    backgroundColor: "#000000",
+    label: "label"
+  }
+};
